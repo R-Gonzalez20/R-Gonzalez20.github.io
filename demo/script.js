a2da8639d221862f930b91342fa3df70d34692dc
R-Gonzalez20 <90006470+R-Gonzalez20@users.noreply.github.com>
2022-03-16 22:33:33 -0400

    Add files via upload

---
+++ demo/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 50, actionDuration:600, jumpHeight: 300, 
+    numOfCollectables: 3,
+    typesOfNPC: 2 }); 
+});
