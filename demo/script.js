81b1585aa4146fa0e40cab2fdf32aa129574531f
R-Gonzalez20 <90006470+R-Gonzalez20@users.noreply.github.com>
2022-03-21 14:05:28 -0400

    Delete demo directory

--- demo/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 50, actionDuration:600, jumpHeight: 300, 
-    numOfCollectables: 3,
-    typesOfNPC: 2 }); 
-});
