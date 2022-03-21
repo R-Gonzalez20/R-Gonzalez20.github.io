218dc6cdce14c190d4c963d136f4f77350934a12
R-Gonzalez20 <90006470+R-Gonzalez20@users.noreply.github.com>
2022-03-21 14:46:16 -0400

    Add files via upload

--- demo/script.js
+++ demo/script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
   mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 50, actionDuration:600, jumpHeight: 400, 
     numOfCollectables: 6,
-    typesOfNPC: 2, pointsPerCollectable: 1, removeCollectableAfterCollected: true, pointsPerNPCInteracted: 2, removeNPCAfterInteracted: true, removeNPCAfterInteractedDelay: 100, playUntilScore: 20, preGameScreenImageURLs: "./img/intro-screen.png", postGameScreenImageURLs: "./img/outro-screen.png"  }); 
+    typesOfNPC: 2, pointsPerCollectable: 1, removeCollectableAfterCollected: true, pointsPerNPCInteracted: 2, removeNPCAfterInteractedDelay: true, removeNPCAfterInteractedDelay: 1000, playUntilScore: 20, preGameScreenImageURLs: "./img/intro-screen.png", postGameScreenImageURLs: "./img/outro-screen.png"  }); 
 });
