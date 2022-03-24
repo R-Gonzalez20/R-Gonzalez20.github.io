d275eea77f6e1809a0e206aa228c4c743273fbfe
R-Gonzalez20 <90006470+R-Gonzalez20@users.noreply.github.com>
2022-03-23 21:36:26 -0400

    Delete demo directory

--- demo/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 50, actionDuration:600, jumpHeight: 400, 
-    numOfCollectables: 6,
-    typesOfNPC: 2, pointsPerCollectable: 1, removeCollectableAfterCollected: true, pointsPerNPCInteracted: 2, removeNPCAfterInteractedDelay: true, removeNPCAfterInteractedDelay: 1000, playUntilScore: 20, preGameScreenImageURLs: "./img/intro-screen.png", postGameScreenImageURLs: "./img/outro-screen.png"  }); 
-});
